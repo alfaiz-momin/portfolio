dade5c16041967b4e2437a12d7412265484c3772
Alfaiz <alfaizmomin@gmail.com>
2017-08-03 15:38:50 +0530

    website completed this is 1.0 version

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,13 +1,10 @@
 import MobileMenu from './modules/MobileMenu';
-// import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
 import StickyHeader from './modules/StickyHeader';
 import ProjectTabs from './modules/Project-tabs';
 
 
 var mobileMenu = new MobileMenu();
-// new RevealOnScroll($(".feature-item"), "20%");
-// new RevealOnScroll($(".testimonial"), "60%");
 var stickyHeader = new StickyHeader();
 var projectTabs = new ProjectTabs();
 
